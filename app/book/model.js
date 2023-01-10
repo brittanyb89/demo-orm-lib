4da94d0ddf18baf243a419eaa6e68b6fdfbd6fe2
Brittany Burton <114821667+brittanyb89@users.noreply.github.com>
2023-01-09 20:18:34 -0600

    Add model for book

---
+++ app/book/model.js
@@ -0,0 +1,38 @@
+import { DataTypes } from "sequelize";
+import sequelize from "../conn.js/";
+
+// add model for Book
+const Book = sequelize.define(
+  "Book",
+  {
+    title: {
+      type: DataTypes.STRING,
+    },
+    author: {
+      type: DataTypes.STRING,
+    },
+    isbn: {
+      type: DataTypes.STRING,
+    },
+    pages: {
+      type: DataTypes.INTEGER,
+    },
+    edition: {
+      type: DataTypes.INTEGER,
+    },
+    // Will become `is_paperback` in table due to `underscored` flag
+    isPaperback: {
+      type: DataTypes.BOOLEAN,
+    },
+  },
+  {
+    // Link to database connection
+    sequelize,
+    // Set to false to remove `created_at` and `updated_at` fields
+    timestamps: false,
+    underscored: true,
+    modelName: "book",
+  }
+);
+
+module.exports = Book;
